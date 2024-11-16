617f69df6b5b395daaf2654d4aa0235d15175458
Bartosz Kruszyński <62513514+Bartosz383@users.noreply.github.com>
2024-11-16 18:54:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 var buttons = querySelectorAll("button");
+
+
+// Pobierz elementy z DOM
+
+
+// Twój klucz API
+
+// Nasłuchiwanie formularza
+
+     // Wywołanie API
+
+    // Wyświetlanie wyniku
