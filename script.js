54766735d9f0a420b4e80bad9025cdc6d16e76e0
jaymitros <jay@mitros.org>
2024-11-16 12:59:02 -0500

    Update script.js
    Dodano kluczAPI i element na forme, miasto i pogode

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-var buttons = querySelectorAll("button");
-
-
 // Pobierz elementy z DOM
-
-
+const buttons = querySelectorAll("button");
+const form = document.getElementById('weather-form');
+const cityInput = document.getElementById('city-input');
+const weatherResult = document.getElementById('weather-result');
 // Twój klucz API
-
+const kluczAPI = "617f1674c75eb7058425b911e7591ae2"
 // Nasłuchiwanie formularza
 
-     // Wywołanie API
+// Wywołanie API
 
-    // Wyświetlanie wyniku
+// Wyświetlanie wyniku
